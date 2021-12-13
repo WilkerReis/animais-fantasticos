9b8f500402c2db8f22349ecc7eb22f0c64ce76c6
Wilker Reis <wilkerdosreis@hotmail.com>
2021-12-13 17:28:58 -0300

    TabNav transformado em class

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,27 +1,34 @@
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-  const tabContent = document.querySelectorAll('[data-tab="content"] section');
+export default class TabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.activeClass = 'ativo';
+  }
 
   // eslint-disable-next-line max-len
   //  se o usuário estiver em outra página que não tem as classes que foram selecionadas no tabMenu e content então vai dar erro no console, para isso testa-se se há ou não antes de executar com o if
 
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove('ativo');
+  activeTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.activeClass);
     });
-    const direcao = tabContent[index].dataset.anime;
-    tabContent[index].classList.add('ativo', direcao);
+    const direcao = this.tabContent[index].dataset.anime;
+    this.tabContent[index].classList.add(this.activeClass, direcao);
   }
 
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo', tabContent[0].dataset.anime);
+  addTabNavEvent() {
     // eslint-disable-next-line max-len
     //  não é o ideal passar assim no addEventListener: activeTab(index), ou passa o nome apenas ou passa dentro da function
 
-    tabMenu.forEach((imagens, index) => {
-      imagens.addEventListener('click', () => {
-        activeTab(index);
-      });
+    this.tabMenu.forEach((imagens, index) => {
+      imagens.addEventListener('click', () => this.activeTab(index));
     });
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      this.activeTab(0);
+      this.addTabNavEvent();
+    }
   }
 }
