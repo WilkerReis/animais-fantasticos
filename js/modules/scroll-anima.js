2c4266d1c0432d16d5e33160dae3ca83a1898374
Wilker Reis <wilkerdosreis@hotmail.com>
2021-12-15 16:50:03 -0300

    animacao ao scroll refeita

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -1,26 +1,38 @@
 export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
-    this.windowMetade = window.innerHeight * 0.7;
+    this.windowMetade = window.innerHeight * 0.6;
 
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top - this.windowMetade;
-      //  console.log(sectionTop);
-      if (sectionTop < 0) {
-        section.classList.add('ativo');
-      } else if (section.classList.contains('ativo')) {
+  getDistances() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
         //  o else aqui faz com que a animação aconteça mesmo depois de já ter acontecido uma vez
-        section.classList.remove('ativo');
+        item.element.classList.remove('ativo');
       }
     });
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener('scroll', this.animaScroll);
+    if (this.sections.length) {
+      this.getDistances();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
   }
 }
